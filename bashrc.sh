0aac3199495434b9a03daecaeb3b7aab6bdf995f
avix133 <avix133@gmail.com>
2025-05-22 12:28:18 +0200

    fix autotmux

--- bashrc.sh
+++ bashrc.sh
@@ -7,7 +7,7 @@ export AWS_REGION=$AWS_DEFAULT_REGION
 export JIRA_HOST="foo"
 
 parse_git_branch() {
-   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+  git branch 2>/dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 export PS1="\u@\h \w\[\033[32m\]\$(parse_git_branch)\[\033[00m\]$ "
 
@@ -15,7 +15,6 @@ alias cdg='cd $(git rev-parse --show-toplevel)'
 alias tfplan='terraform plan -out plan.tfplan'
 alias tfplano='tempfile="$(mktemp)" && tfplan -no-color | tee "${tempfile}" && echo "Saved to ${tempfile}" && code ${tempfile}'
 alias tfapply='terraform apply plan.tfplan'
-
 
 alias aws_tr_secrets='pbpaste | sed "s/export //g" | tr -d \" | pbcopy'
 
@@ -25,10 +24,8 @@ alias aws_sqs='aws sqs list-queues --output json | jq -r ".QueueUrls[]" | fzf --
 alias aws_secretsmanager='aws secretsmanager list-secrets --output json | jq -r ".SecretList[].Name" | fzf --sync --exit-0 | xargs -I {} aws secretsmanager get-secret-value --secret-id {} --output json | jq'
 alias aws_ecr='aws ecr describe-repositories --output json | jq -r ".repositories[].repositoryName" | fzf --sync --exit-0 | xargs -I {} aws ecr describe-images --repository-name {} --output json | jq'
 
-
 [ -f /usr/local/etc/bash_completion ] && source /usr/local/etc/bash_completion
 source <(kubectl completion bash)
-
 
 # allow locally installed npm binaries to be executed;
 # added by `npm i -g add-local-binaries-to-path`
@@ -42,14 +39,13 @@ function git_pr_checkout() {
 }
 
 # tmux
-[ -z "$TMUX"  ] && { tmux attach -t "$(pwd)" || exec tmux new-session -s "$(pwd)";}
-alias reload_tmux='tmux source-file ~/.tmux.conf'
+pwd_tmux_session="$(pwd | sed 's|\.|_|g')"
+[ ! "$TERM_PROGRAM" = "tmux" ] && { tmux attach -t "$pwd_tmux_session" || exec tmux new-session -s "$pwd_tmux_session";}
 
 eval "$(thefuck --alias)"
 eval "$(fzf --bash)"
 source $HOME/custom-bash/fzf-git.sh/fzf-git.sh
 source $HOME/custom-bash/fzf-docker/docker-fzf
-
 
 export FZF_TMUX=1
 export FZF_TMUX_OPTS='-p80%,60% --height=50%'
@@ -60,16 +56,14 @@ function fzf_aws_profile {
 }
 
 function aws_sso_default_profile {
-    aws sts get-caller-identity || aws sso login --profile "$AWS_DEFAULT_PROFILE"
+  aws sts get-caller-identity || aws sso login --profile "$AWS_DEFAULT_PROFILE"
 }
 
 alias aws_profile='export AWS_DEFAULT_PROFILE="$(fzf_aws_profile)" && aws_sso_default_profile'
 
-
-
 export TODAYS_JIRA_PATH="$HOME/.jira-$(date '+%Y-%m-%d').json"
 function jira_remove_cache {
-    find $HOME -maxdepth 1 -name ".jira*" | xargs -I {} rm {}
+  find $HOME -maxdepth 1 -name ".jira*" | xargs -I {} rm {}
 }
 
 function get_active_jira_tickets {
